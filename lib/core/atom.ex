50004b9ea95df0c5475eb016e59532b0c56d7f28
Eric Bouchut <ebouchut@gmail.com>
2018-05-27 21:42:47 +0200

    Add Unit Tests for Atom

---
+++ lib/core/atom.ex
@@ -0,0 +1,17 @@
+defmodule Core.Atom do
+  @moduledoc """
+  Learn Elixir Atom class by example.
+
+  ## Examples
+
+      iex> is_atom(:name)
+      true
+
+      iex> Atom.to_string(:name)
+      "name"
+
+      iex> Atom.to_char_list(:eric)
+      'eric'
+  """
+
+end
---
+++ test/core/atom_test.exs
@@ -0,0 +1,5 @@
+defmodule Core.AtomTest do
+  use ExUnit.Case, async: true
+  doctest Core.Atom
+
+end
